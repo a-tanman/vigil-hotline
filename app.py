3b634086f5c21cb0cc5476d5427631b9324afa68
Andrew Tan <andrewtan89@gmail.com>
2018-10-21 01:56:45 +0800

    working version, use ngrok to run client

--- app.py
+++ app.py
@@ -2,18 +2,33 @@
 # Imports
 #----------------------------------------------------------------------------#
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from datetime import datetime
+from flask_cors import CORS
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import math
 # from flask.ext.sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from forms import *
 import os
 
+import io
+
+# Imports the Google Cloud client library
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+# Instantiates a client
+client = speech.SpeechClient()
+
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object('config')
 #db = SQLAlchemy(app)
 
@@ -36,6 +51,22 @@ def login_required(test):
             return redirect(url_for('login'))
     return wrap
 '''
+
+# Create list of calls
+calls = [
+    {
+        'time': str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')),
+        'text': 'Help!',
+        'sentiment': 7, 
+        'confidence': 8
+    },
+    {
+        'time': str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')),
+        'text': 'I\'m lonely.',
+        'sentiment': 5, 
+        'confidence': 9
+    }
+]
 #----------------------------------------------------------------------------#
 # Controllers.
 #----------------------------------------------------------------------------#
@@ -43,7 +74,8 @@ def login_required(test):
 
 @app.route('/')
 def home():
-    return render_template('pages/placeholder.home.html')
+    calls.sort(key = lambda x: x['sentiment'], reverse = True)
+    return render_template('pages/placeholder.home.html', calls_data = calls)
 
 
 @app.route('/about')
@@ -68,6 +100,11 @@ def forgot():
     form = ForgotForm(request.form)
     return render_template('forms/forgot.html', form=form)
 
+@app.route('/recorder')
+def recorder():
+    form = ForgotForm(request.form)
+    return render_template("Recorderjs-master/examples/example_simple_exportwav.html", form=form)
+
 # Error handlers.
 
 
@@ -91,17 +128,67 @@ if not app.debug:
     app.logger.addHandler(file_handler)
     app.logger.info('errors')
 
+# List for request from client
+
+@app.route('/api/newcall', methods = ['POST'])
+def create_call():
+    
+    # if not request.json:
+    #     abort(400)
+        
+    # return redirect(url_for('login'))
+
+    blob = request.files['audio_data'].read()
+    # blob.save(os.path.join())
+    text = transcribe(blob)
+    print(text.transcript)
+    print(text.confidence)
+
+    analyzer = SentimentIntensityAnalyzer()
+    vs = analyzer.polarity_scores(text.transcript)
+    print("{:-<65} {}".format(text.transcript, str(vs)))
+    
+    
+    call = {
+        'id': str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')),
+        'text': text.transcript,
+        'sentiment': round(10*vs['neg']),
+        'confidence': round(10*text.confidence)
+    }
+    calls.append(call)
+
+    return jsonify({'call': call}), 201
+
+def transcribe(blob):
+    
+    client = speech.SpeechClient()
+    content = blob
+    audio = types.RecognitionAudio(content=content)
+
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=48000,
+        language_code='en-US')
+
+    # Detects speech in the audio file
+    response = client.recognize(config, audio)
+
+    for result in response.results:
+        print(result)
+        print('Transcript: {}'.format(result.alternatives[0].transcript))
+
+    return(result.alternatives[0])
 #----------------------------------------------------------------------------#
 # Launch.
 #----------------------------------------------------------------------------#
 
 # Default port:
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
 
 # Or specify port manually:
-'''
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=port)
-'''
+
